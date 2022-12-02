014f6b843cfcdc11859be0de4a997f477fc4cd62
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-12-02 18:27:06 -0500

    [Backend] User Delete

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,4 +1,5 @@
 import UserModel from "../Models/userModel.js";
+import bcrypt from "bcrypt";
 
 export const getUser  = async (req, res)=>{
     const id = req.params.id;
@@ -23,10 +24,13 @@ export const updateUser = async (req, res) => {
     const { currentUserId, currentUserAdminStatus, password } = req.body;
 
     if (id === currentUserId || currentUserAdminStatus) {
+        if(password){
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(password, salt)
+
+        }
         try {
-            const user = await UserModel.findByIdAndUpdate(id, req.body, {
-                new: true,
-            });
+            const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
 
             res.status(200).json(user);
         } catch (error) {
@@ -34,5 +38,24 @@ export const updateUser = async (req, res) => {
         }
     } else {
         res.status(403).json("Access Denied! you can only update your own profile");
+    }
+};
+
+
+// Delete user
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+
+    const { currentUserId, currentUserAdminStatus } = req.body;
+
+    if (currentUserId === id || currentUserAdminStatus) {
+        try {
+            await UserModel.findByIdAndDelete(id);
+            res.status(200).json("User deleted successfully");
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("Access Denied! you can only delete your own profile");
     }
 };
