16eba437a4ea1c63119612fdec17e91aaf86ee27
Jingyu-An <jqrohan@gmail.com>
2022-12-03 00:34:00 -0500

    [Backend] Post Like/Unlike

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -70,3 +70,27 @@ export const deletePost = async (req, res) => {
     
   }
 }
+
+// Like/Dislike
+export const likePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+  
+  try {
+    const post = await PostModel.findById(id);
+    if (!post.likes.includes(userId)) {
+      await post.updateOne({
+        $push: {likes: userId}
+      });
+      res.status(200).json("Post liked");
+    } else {
+      await post.updateOne({
+        $pull: {likes: userId}
+      });
+      res.status(200).json("Post unliked");
+    }
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createPost, getPost, updatePost, deletePost} from "../Controllers/PostController.js";
+import {createPost, getPost, updatePost, deletePost, likePost} from "../Controllers/PostController.js";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/', createPost);
 router.get('/:id', getPost);
 router.put('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.put('/:id/like', likePost);
 
 export default router;
