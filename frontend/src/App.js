379c3783c3b845955a938f865076bf550f16753e
Niloo KSZ <niloofar.keshavarz@gmail.com>
2022-12-05 16:19:26 -0500

    Install bootstrap and creates Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css"
+import Navbar from './components/Navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
 //import Home from "./pages/home/Home";
 //import Profile from "./pages/profile/Profile";
 //import Auth from "./pages/auth/Auth";
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
          <div className="blur" style={{top: '-18%', right: '0'}}></div>
         <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
+        <Navbar/>
         {/* <Home /> */}
         {/* <Profile/> */}
         {/* <Auth/> */}
