feb0a30bb154d93ec82e992a85815f7c6ded61d7
Niloo KSZ <niloofar.keshavarz@gmail.com>
2022-12-05 14:53:06 -0500

    HomePage setting and color theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,18 @@
-//import logo from './logo.svg';
-//import './App.css';
+import "./App.css"
+//import Home from "./pages/home/Home";
+//import Profile from "./pages/profile/Profile";
+//import Auth from "./pages/auth/Auth";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         
-        </a>
-      </header>
+         <div className="blur" style={{top: '-18%', right: '0'}}></div>
+        <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
+        {/* <Home /> */}
+        {/* <Profile/> */}
+        {/* <Auth/> */}
     </div>
+    
   );
 }
 
