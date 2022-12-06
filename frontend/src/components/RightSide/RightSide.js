a113077c7cdd74bd54065ad227a87aad28b4bc63
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-12-05 22:13:18 -0500

    rightside function updated

--- frontend/src/components/RightSide/RightSide.js
+++ frontend/src/components/RightSide/RightSide.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './RightSide.css';
-import Home from '../../img/home.png';
-import Noti from '../../img/noti.png';
-import Comment from '../../img/comment.png';
-//import {UilSetting} from '@iconscout/react-unicons';
-import TrendCard from '../trendCard/TrendCard';
+
+//import TrendCard from '../trendCard/TrendCard';
 
 
 const RightSide = () => {
