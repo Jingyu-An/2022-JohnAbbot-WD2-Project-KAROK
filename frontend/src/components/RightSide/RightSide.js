6f6a504219fd450ea5055d9128ceb20259d8ea05
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-12-05 22:05:48 -0500

    RightSide function created

---
+++ frontend/src/components/RightSide/RightSide.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './RightSide.css';
+import Home from '../../img/home.png';
+import Noti from '../../img/noti.png';
+import Comment from '../../img/comment.png';
+//import {UilSetting} from '@iconscout/react-unicons';
+import TrendCard from '../trendCard/TrendCard';
+
+
+const RightSide = () => {
+  return (
+   <div className="RightSide">
+     RightSide
+   </div>
+  )
+}
+export default RightSide;
