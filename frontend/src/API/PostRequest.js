258d7d8e8b79ff7b1160e5cbfd1e95107c84189c
Jingyu-An <jqrohan@gmail.com>
2022-12-06 18:47:35 -0500

    [Frontend] Add like/unlike function with db

--- frontend/src/API/PostRequest.js
+++ frontend/src/API/PostRequest.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 const API = axios.create({baseURL: "http://localhost:5002"})
 
 export const getTimelinePosts= (id)=> API.get(`/posts/${id}/timeline`);
+export const likePost = (id, userId)=> API.put(`/posts/${id}/like`, {
+  userId: userId
+})
--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -6,10 +6,21 @@ import Heart from '../../img/like.png';
 import NotLike from '../../img/notlike.png';
 import Comments from "../Comment/Comments";
 import {useSelector} from "react-redux";
+import {likePost} from "../../API/PostRequest";
 
 const Post = ({data}) => {
   
   const {user} = useSelector((state) => state.authReducer.authData);
+  
+  const [liked, setLiked] = useState(data.likes.includes(user.id));
+  const [likes, setLikes] = useState(data.likes.length);
+  
+  const likeHandler = () => {
+    setLiked((prev) => !prev);
+    likePost(data._id, user._id);
+    liked ? setLikes((prev) => prev - 1)
+      : setLikes((prev) => prev + 1);
+  };
   
   const [enableComment, setEnableComment] = useState(false);
   
@@ -30,9 +41,10 @@ const Post = ({data}) => {
       />
       <div className='postReact'>
         <img
-          src={data.liked ? Heart : NotLike}
+          src={liked ? Heart : NotLike}
           alt=""
           style={{cursor: "pointer"}}
+          onClick={likeHandler}
         />
         <img
           src={Comment}
@@ -42,7 +54,7 @@ const Post = ({data}) => {
         />
         {/*<img src = {Share} alt = ""/>*/}
       </div>
-      <span style={{color: "var(--gray)", fontSize: '12px'}}>{data.likes}likes</span>
+      <span style={{color: "var(--gray)", fontSize: '12px'}}>{likes}likes</span>
       <div className='detail'>
         <span><b>{data.name}</b></span>
         <span> {data.desc}</span>
