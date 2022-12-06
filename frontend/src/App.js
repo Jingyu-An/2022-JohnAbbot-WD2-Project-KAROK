cfebcfd6179d30ce8ee7082543b538fa9f8a75ec
Niloo KSZ <niloofar.keshavarz@gmail.com>
2022-12-05 23:57:23 -0500

    Adds Authentication form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Home from "./pages/home/Home";
 
 import Profile from "./pages/profile/Profile";
-//import Auth from "./pages/auth/Auth";
+import Auth from "./pages/auth/Auth";
 
 function App() {
   return (
     <div className="App">
          <div className="blur" style={{top: '-18%', right: '0'}}></div>
         <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
-        <Navbar/>
+        {/* <Navbar/>
         <Home />
-         <Profile/> 
-        {/* <Auth/> */}
+         <Profile/>  */}
+         <Auth/> 
     </div>
     
   );
