37b0518177d351ab7c146cb097cd6b09933d76ae
Niloo KSZ <niloofar.keshavarz@gmail.com>
2022-12-05 23:18:38 -0500

    Imports Post side and right side to home

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Profile.css';
-import ProfileLeft from '../../components/profileLeft/ProfileLeft' ;
+//import ProfileLeft from '../../components/profileLeft/ProfileLeft' ;
 import ProfileCard from '../../components/profileCard/ProfileCard' ;
 import PostSide from '../../components/postSide/PostSide' ;
 import RightSide from '../../components/rightSide/RightSide' ;
@@ -10,7 +10,7 @@ const Profile = () => {
   return (
    <div className="Profile">
 
-       <ProfileLeft/>
+       {/* <ProfileLeft/> */}
        <div className="Profile-center">
             <ProfileCard/>
             <PostSide/>
