da5780a207a43cb9fc6c04e0e1efe59f584df9cd
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-12-07 14:42:17 -0500

    [FrontEnd] User and Follower/Following connect to Backend

--- frontend/src/components/InfoCard/InfoCard.js
+++ frontend/src/components/InfoCard/InfoCard.js
@@ -1,41 +1,64 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './InfoCard.css';
 import {UilPen} from '@iconscout/react-unicons';
-import { useState } from 'react';
-import ProfileModal  from '../ProfileModal/ProfileModal.js';
+import {useState} from 'react';
+import ProfileModal from '../ProfileModal/ProfileModal.js';
 import LogOut from "../Logout/LogOut";
+import {useParams} from "react-router-dom";
+import * as UserApi from "../../API/UserRequests.js";
+import {useDispatch, useSelector} from "react-redux";
 
 const InfoCard = () => {
+  const [modalOpened, setModalOpened] = useState(false);
+  const params = useParams();
+  const dispatch = useDispatch();
+  const profileUserId = params.id;
+  const [profileUser, setProfileUser] = useState({});
+  const {user} = useSelector((state) => state.authReducer.authData);
 
-    const [modalOpened , setModalOpened] = useState(false);
+  useEffect(() => {
+
+      const fetchProfileUser = async () => {
+        if (profileUserId === user._id) {
+          setProfileUser(user)
+        }else{
+          console.log("fetching");
+          const profileUser = await UserApi.getUser(profileUserId);
+          setProfileUser(profileUser);
+        }
+      }
+    },[user] );
+
   return (
-   <div className="InfoCard">
-        <div  className='infoHead'>
-            <h4>Your Information</h4>
-            <div>
-            <UilPen width = '2rem' height ='1.2rem' onClick = {() => setModalOpened(true)}/>
-            <ProfileModal
-                modalOpened ={modalOpened}
-                setModalOpened ={setModalOpened}
-            />
-            </div>
+    <div className="InfoCard">
+      <div className='infoHead'>
+        <h4>Your Information</h4>
+        <div>
+          <UilPen width='2rem' height='1.2rem' onClick={() => setModalOpened(true)}/>
+          <ProfileModal
+            modalOpened={modalOpened}
+            setModalOpened={setModalOpened}
+            data ={user}
+          />
         </div>
-        <div className='info'>
-            <span><b>Status </b></span>
-            <span>In Relationship</span>
-        </div>
-        <div className='info'>
-            <span><b>Lives in </b></span>
-            <span>Montreal</span>
-        </div>
-        <div className='info'>
-            <span><b>Works at </b></span>
-            <span>Montreal Company</span>
-        </div>
+      </div>
+      <div className='info'>
+        <span><b>Status </b></span>
+        <span>{profileUser.relationship}</span>
+      </div>
+      <div className='info'>
+        <span><b>Lives in </b></span>
+        <span>{profileUser.liveIn}</span>
+      </div>
+      <div className='info'>
+        <span><b>Works at </b></span>
+        <span>{profileUser.worksAt}</span>
+      </div>
       <LogOut/>
-       
-   </div>
+
+    </div>
   )
 }
+
 
 export default InfoCard;
--- frontend/src/components/ProfileModal/ProfileModal.js
+++ frontend/src/components/ProfileModal/ProfileModal.js
@@ -1,23 +1,85 @@
-import { Modal, useMantineTheme } from '@mantine/core';
+import React, {useState} from "react";
+import {Modal, useMantineTheme} from "@mantine/core";
+import {useDispatch, useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
+import {uploadImage} from "../../Actions/uploadAction";
+import {updateUser} from "../../API/UserRequests";
+
 //mantine is a library and this Modal is of its components
 
-function ProfileModal({modalOpened , setModalOpened}) {
+const ProfileModal = ({ modalOpened, setModalOpened, data }) => {
   const theme = useMantineTheme();
+  const{password, ...other} = data
+  const [formData, setFormData] = useState(other);
+  const [profileImage, setProfileImage] = useState(null);
+  const [coverImage, setCoverImage] = useState(null);
+  const dispatch = useDispatch();
+  const param = useParams();
+
+  const {user} = useSelector((state) => state.authReducer.authData)
+
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
+  const onImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      let img = e.target.files[0];
+      e.target.name === "profileImage"
+        ? setProfileImage(img)
+        : setCoverImage(img)
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let UserData = formData;
+    if (profileImage) {
+      const data = new FormData();
+      const fileName = Date.now() + profileImage.name;
+      data.append("name", fileName);
+      data.append("file", profileImage)
+      UserData.profilePicture = fileName;
+      try {
+        dispatch(uploadImage(data));
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    if (coverImage) {
+      const data = new FormData();
+      const fileName = Date.now() + coverImage.name;
+      data.append("name", fileName);
+      data.append("file", coverImage)
+      UserData.coverPicture = fileName;
+      try {
+        dispatch(uploadImage(data));
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    dispatch(updateUser(param.id, UserData));
+    setModalOpened(false);
+  }
+
 
   return (
     <Modal
       overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
       overlayOpacity={0.55}
       overlayBlur={3}
-      opened= {modalOpened}
-      size = "auto"
-      onClose = {() => setModalOpened(false)}
+      opened={modalOpened}
+      size="auto"
+      onClose={() => setModalOpened(false)}
     >
-       <form className="infoForm">
+      <form className="infoForm">
         <h3>Your info</h3>
 
         <div>
-          <input
+          <inputi
+            value={formData.firstname}
+            onChange={handleChange}
             type="text"
             className="infoInput"
             name="FirstName"
@@ -25,6 +87,8 @@ function ProfileModal({modalOpened , setModalOpened}) {
           />
 
           <input
+            value={formData.lastname}
+            onChange={handleChange}
             type="text"
             className="infoInput"
             name="LastName"
@@ -34,6 +98,8 @@ function ProfileModal({modalOpened , setModalOpened}) {
 
         <div>
           <input
+            value={formData.worksAt}
+            onChange={handleChange}
             type="text"
             className="infoInput"
             name="worksAT"
@@ -43,6 +109,8 @@ function ProfileModal({modalOpened , setModalOpened}) {
 
         <div>
           <input
+            value={formData.livesIn}
+            onChange={handleChange}
             type="text"
             className="infoInput"
             name="livesIN"
@@ -50,6 +118,8 @@ function ProfileModal({modalOpened , setModalOpened}) {
           />
 
           <input
+            value={formData.country}
+            onChange={handleChange}
             type="text"
             className="infoInput"
             name="Country"
@@ -59,6 +129,8 @@ function ProfileModal({modalOpened , setModalOpened}) {
 
         <div>
           <input
+            value={formData.relationship}
+            onChange={handleChange}
             type="text"
             className="infoInput"
             placeholder="RelationShip Status"
@@ -67,15 +139,16 @@ function ProfileModal({modalOpened , setModalOpened}) {
 
 
         <div>
-            Profile Image 
-            <input type="file" name='profileImg'/>
-            Cover Image
-            <input type="file" name="coverImg" />
+          Profile Image
+          <input type="file" name='profileImg'/>
+          Cover Image
+          <input type="file" name="coverImg"/>
         </div>
 
         <button className="button infoButton">Update</button>
       </form>
     </Modal>
   );
-} 
+}
+
 export default ProfileModal;
