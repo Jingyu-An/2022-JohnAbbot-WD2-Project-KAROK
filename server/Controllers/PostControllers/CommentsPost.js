60e62df66ad960ab9c799384fb7ab277a1b6832f
Jingyu-An <jqrohan@gmail.com>
2022-12-07 11:46:48 -0500

    [Frontend] Fix comment delete issue

--- server/Controllers/PostControllers/CommentsPost.js
+++ server/Controllers/PostControllers/CommentsPost.js
@@ -20,15 +20,17 @@ export const getCommentPost = async (req, res) => {
 // Add Comments
 export const addCommentPost = async (req, res) => {
   const id = req.params.id;
-  const {userId, comments} = req.body;
+  const {username, userId, comments, commentId} = req.body;
   
   try {
     const post = await PostModel.findById(id);
     await post.updateOne({
       $push: {
         comments: {
-          userId : userId,
-          comment: comments
+          username : username,
+          userId   : userId,
+          comment  : comments,
+          commentId: commentId,
         }
       }
     });
@@ -53,8 +55,8 @@ export const deleteCommentPost = async (req, res) => {
     await post.updateOne({
       $pull: {
         comments: {
-          _id   : comId,
-          userId: userId,
+          commentId: comId,
+          userId   : userId,
         }
       }
     });
