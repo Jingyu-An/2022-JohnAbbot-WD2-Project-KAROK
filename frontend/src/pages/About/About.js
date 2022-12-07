d777060e736acb9b63e80d68a6c8c43bb37cae27
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-12-06 22:17:00 -0500

    about design updated

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './About.css'
+import '../../App.css'
 
 import image1 from '../../img/img1.png';
 import image3 from '../../img/img3.png';
@@ -18,10 +19,10 @@ const About = () => {
     <Card style={{ width: '15rem' }}>
     
     <Card.Body>
-    <img className='rounded' src={image3} alt="" />
+    <img className='rounded image' src={image3} alt="" />
     <br/>
     
-      <Card.Title><b>Nupur Kesarwani</b><br/>
+      <Card.Title className='title'><b>Nupur Kesarwani</b><br/>
       Lead Developer
       </Card.Title>
 
@@ -29,49 +30,49 @@ const About = () => {
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
       </Card.Text>
-      <Button variant="success">Contact</Button>
+      <Button variant="success" className="button">Contact</Button>
     </Card.Body>
   </Card>
   <Card style={{ width: '15rem' }}>
     
     <Card.Body>
-    <img className='rounded' src={image3} alt="" />
-      <Card.Title><b>Niloofar Keshavarz</b><br/>
+    <img className='rounded image' src={image3} alt="" />
+      <Card.Title className='title'><b>Niloofar Keshavarz</b><br/>
       Lead Developer
       </Card.Title>
       <Card.Text>
         Some quick example text to build on the card title aand make up the
         bulk of the card's content.
       </Card.Text>
-      <Button variant="success">Contact</Button>
+      <Button variant="success" className="button">Contact</Button>
     </Card.Body>
   </Card>
   <Card style={{ width: '15rem' }}>
    
     <Card.Body>
-    <img className='rounded' src={image1} alt="" />
-      <Card.Title><b>Jingyu An</b><br/>
+    <img className='rounded image' src={image1} alt="" />
+      <Card.Title className='title'><b>Jingyu An</b><br/>
       Lead Developer
       </Card.Title>
       <Card.Text>
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
       </Card.Text>
-      <Button variant="success">Contact</Button>
+      <Button variant="success" className="button">Contact</Button>
     </Card.Body>
   </Card>
   <Card style={{ width: '15rem' }}>
     
     <Card.Body>
-    <img className='rounded' src={image3} alt="" />
-      <Card.Title><b>Hyunju Roh</b><br/>
+    <img className='rounded image' src={image3} alt="" />
+      <Card.Title className='title'><b>Hyunju Roh</b><br/>
       Lead Developer
       </Card.Title>
       <Card.Text>
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
       </Card.Text>
-      <Button variant="success">Contact</Button>
+      <Button variant="success" className="button">Contact</Button>
     </Card.Body>
   </Card>
   </div>
